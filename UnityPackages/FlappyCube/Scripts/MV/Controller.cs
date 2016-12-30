af0c3655132d8e553bbcaeaf9928e39c4d297ca7
Eugen Mirovschi <emirovschi@vmists.com>
2016-12-30 21:19:55 +0200

    Adding MV interfaces

---
+++ UnityPackages/FlappyCube/Scripts/MV/Controller.cs
@@ -0,0 +1,17 @@
+﻿public abstract class Controller<T> where T : IView
+{
+	private readonly T view;
+
+	public T View
+	{
+		get
+		{
+			return view;
+		}
+	}
+
+	public Controller(T view)
+	{
+		this.view = view;
+	}
+}
---
+++ UnityPackages/FlappyCube/Scripts/MV/ControllerFactory.cs
@@ -0,0 +1,17 @@
+﻿using Zenject;
+
+public class ControllerFactory<TC, TV> : IFactory<TC> where TC : Controller<TV> where TV : IView
+{
+	private readonly DiContainer container;
+
+	public ControllerFactory(DiContainer container)
+	{
+		this.container = container;
+	}
+
+	public TC Create()
+	{
+		return container.Resolve<TC>();
+	}
+}
+

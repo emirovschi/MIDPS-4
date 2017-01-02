cbfe48165ec7917ce217a91e7f2bd07e4cf0ac4a
Eugen Mirovschi <emirovschi@vmists.com>
2017-01-02 17:59:23 +0200

    Adding counter

---
+++ UnityPackages/FlappyCube/Scripts/Obstacles/Counter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class Counter : MonoBehaviour
+{
+    private Player player;
+    private AddScoreSignal addScoreSignal;
+
+    [Inject]
+    public void Initialize(Player player, AddScoreSignal addScoreSignal)
+    {
+        this.player = player;
+        this.addScoreSignal = addScoreSignal;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject == player.gameObject)
+        {
+            addScoreSignal.Fire();
+        }
+    }
+}
